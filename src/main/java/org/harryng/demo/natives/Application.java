a4eedb34b9c9781fdbd096ed9512f42bc6e57f15
harryng <harry@videabiz.com>
2022-04-06 16:32:06 +0700

    - h2 git can not be compiled to native

--- src/main/java/org/harryng/demo/natives/Application.java
+++ src/main/java/org/harryng/demo/natives/Application.java
@@ -11,14 +11,17 @@ public class Application {
     static System.Logger logger = System.getLogger(Application.class.getCanonicalName());
 //    static Logger logger = LoggerFactory.getLogger(Application.class);
 
+    public static void reflect() throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+        Db db = new Db();
+        Method insertDbMethod = db.getClass().getMethod("insertDb");
+        logger.log(System.Logger.Level.INFO, "reflection invoked");
+        insertDbMethod.invoke(db);
+    }
+
     public static void main(String[] args) throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
         logger.log(System.Logger.Level.INFO, "=====" + ResourcesUtil.getProperty("db.jdbc.driver"));
 //        logger.info("=====");
         Db db = new Db();
-        Method insertDbMethod = db.getClass().getMethod("insertDb");
-//        db.insertDb();
-        logger.log(System.Logger.Level.INFO, "reflection invoked");
-        insertDbMethod.invoke(db);
-
+        db.insertDb();
     }
 }
