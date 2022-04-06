1700b733d9143b6a667cda85490d89e155fec750
harryng <harry@videabiz.com>
2022-04-06 12:15:28 +0700

    Initial commit

---
+++ setup/build-template.sh
@@ -0,0 +1,27 @@
+native-image \
+-J-Dsun.nio.ch.maxUpdateArraySize=100 \
+-J-Djava.util.logging.manager=org.jboss.logmanager.LogManager \
+-J-DCoordinatorEnvironmentBean.transactionStatusManagerEnable=false \
+-J-Dvertx.logger-delegate-factory-class-name=io.quarkus.vertx.core.runtime.VertxLogDelegateFactory \
+-J-Dvertx.disableDnsResolver=true \
+-J-Dio.netty.leakDetection.level=DISABLED \
+-J-Dio.netty.allocator.maxOrder=3 \
+-J-Duser.language=vi \
+-J-Duser.country=VN \
+-J-Dfile.encoding=UTF-8 \
+-H:-ParseOnce \
+-J--add-exports=java.security.jgss/sun.security.krb5=ALL-UNNAMED \
+-J--add-opens=java.base/java.text=ALL-UNNAMED \
+--gc=G1 \
+-H:InitialCollectionPolicy=com.oracle.svm.core.genscavenge.CollectionPolicy\$BySpaceAndTime \
+-H:+JNI \
+-H:+AllowFoldMethods \
+-J-Djava.awt.headless=true \
+-H:FallbackThreshold=0 \
+-H:+ReportExceptionStackTraces \
+-H:-AddAllCharsets \
+-H:EnableURLProtocols=http,https \
+-H:NativeLinkerOption=-no-pie \
+-H:-UseServiceLoaderFeature \
+-H:+StackTrace demo-quarkus-1.0-SNAPSHOT-runner \
+-jar demo-quarkus-1.0-SNAPSHOT-runner.jar
---
+++ src/main/java/org/harryng/demo/natives/Application.java
@@ -0,0 +1,13 @@
+package org.harryng.demo.natives;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Application {
+
+    static Logger logger = LoggerFactory.getLogger(Application.class);
+
+    public static void main(String[] args){
+        logger.info("hello world");
+    }
+}
