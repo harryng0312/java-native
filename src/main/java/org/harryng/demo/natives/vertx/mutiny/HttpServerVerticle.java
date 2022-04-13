2fb73045cca483c957bd1b75ddc796e2ec118d5f
harryng <harry@videabiz.com>
2022-04-13 16:14:43 +0700

    - tcp socket

--- src/main/java/org/harryng/demo/natives/vertx/mutiny/HttpServerVerticle.java
+++ src/main/java/org/harryng/demo/natives/vertx/mutiny/HttpServerVerticle.java
@@ -2,12 +2,14 @@ package org.harryng.demo.natives.vertx.mutiny;
 
 import io.smallrye.mutiny.Uni;
 import io.smallrye.mutiny.vertx.core.AbstractVerticle;
-import io.vertx.core.http.HttpMethod;
 import io.vertx.core.http.HttpServerOptions;
 import io.vertx.core.json.JsonObject;
 import io.vertx.mutiny.core.http.HttpServer;
 import io.vertx.mutiny.ext.web.Router;
 import io.vertx.mutiny.ext.web.RoutingContext;
+
+import java.nio.charset.StandardCharsets;
+import java.util.concurrent.atomic.AtomicReference;
 
 public class HttpServerVerticle extends AbstractVerticle {
     static System.Logger logger = System.getLogger(HttpServerVerticle.class.getCanonicalName());
@@ -24,15 +26,26 @@ public class HttpServerVerticle extends AbstractVerticle {
                 .put("name", name)
                 .put("address", address)
                 .put("message", "Hello " + name + " connected from " + address));
-//                    context.response()
-////                            .putHeader("content-type", "application/json")
-//                            .end(new JsonObject()
-//                                    .put("name", name)
-//                                    .put("address", address)
-//                                    .put("message", "Hello " + name + " connected from " + address)
-//                                    .toString())
-//                            .subscribe().with(v -> {
-//                            });
+    }
+
+    public void onPostHello(RoutingContext context) {
+        String address = context.request().connection().remoteAddress().hostAddress()
+                + " " + context.request().connection().remoteAddress().port();
+        var queryParams = context.queryParams();
+        var id = context.pathParam("id");
+        var name = queryParams.contains("name") ? queryParams.get("name") : "unknown";
+        context.request().body().map(buffer -> {
+            var reqData = new JsonObject(buffer.getDelegate());
+            var resData = new JsonObject()
+                    .put("id", id)
+                    .put("name", name)
+                    .put("address", address)
+                    .put("message", "Hello " + name + " connected from " + address)
+                    .put("requestData", reqData);
+            return resData;
+        }).flatMap(context::json
+        ).subscribe().with(itm -> {
+        });
     }
 
     public void onFailure(RoutingContext context) {
@@ -47,14 +60,21 @@ public class HttpServerVerticle extends AbstractVerticle {
         var rootRouter = Router.router(vertx);
         rootRouter.route("/");
 
-        var helloRouter = Router.router(vertx);
-        helloRouter.get("/:id")
+        var getHelloRouter = Router.router(vertx);
+        var postHelloRouter = Router.router(vertx);
+        getHelloRouter.get("/:id")
                 .produces("application/json")
                 .consumes("application/json")
                 .handler(this::onGetHello)
                 .failureHandler(this::onFailure);
+        postHelloRouter.post("/:id")
+                .produces("application/json")
+                .consumes("application/json")
+                .handler(this::onPostHello)
+                .failureHandler(this::onFailure);
 
-        rootRouter.mountSubRouter("/hello", helloRouter);
+        rootRouter.mountSubRouter("/hello", getHelloRouter);
+        rootRouter.mountSubRouter("/hello", postHelloRouter);
         return server
                 .requestHandler(rootRouter)
                 .listen(8080)
--- src/test/java/org/harryng/demo/natives/vertx/mutiny/HttpServerVerticleTest.java
+++ src/test/java/org/harryng/demo/natives/vertx/mutiny/HttpServerVerticleTest.java
@@ -11,7 +11,7 @@ import java.nio.charset.StandardCharsets;
 public class HttpServerVerticleTest extends AbstractVertxTest {
 
     @Test
-    public void testSendHttpServerFromClient() {
+    public void testGetHttpServerFromClient() {
         var options = new HttpClientOptions();
         var client = vertx.createHttpClient(options);
         var reqOptions = new RequestOptions()
@@ -25,6 +25,33 @@ public class HttpServerVerticleTest extends AbstractVertxTest {
                 }).flatMap(HttpClientRequest::connect)
                 .map(httpClientResponse -> httpClientResponse.bodyHandler(buffer -> {
                     logger.log(System.Logger.Level.INFO, "Receive:" + new String(buffer.getBytes(), StandardCharsets.UTF_8));
+                }))
+                .onFailure().invoke(ex -> logger.log(System.Logger.Level.ERROR, "Ex:", ex))
+                .onItemOrFailure().invoke((itm, ex) -> client.closeAndForget());
+        vertx.executeBlockingAndAwait(req);
+    }
+
+    @Test
+    public void testPostHttpServerFromClient() {
+        var options = new HttpClientOptions();
+        var client = vertx.createHttpClient(options);
+        var reqOptions = new RequestOptions()
+                .setHost("localhost").setPort(8080)
+                .addHeader("content-type", "application/json");
+        var reqData = "{\n" +
+                "    \"id\":1,\n" +
+                "    \"name\":\"test sub 1\"\n" +
+                "}";
+        var req = client
+                .request(reqOptions.setURI("/hello/1?name=test01").setMethod(HttpMethod.POST))
+                .map(httpClientRequest -> {
+                    logger.log(System.Logger.Level.INFO, "Pushing data...");
+                    return httpClientRequest;
+                }).flatMap(httpClientRequest -> httpClientRequest.send(reqData))
+                .map(httpClientResponse -> httpClientResponse.bodyHandler(buffer -> {
+                    logger.log(System.Logger.Level.INFO, "Receive body:" + new String(buffer.getBytes(), StandardCharsets.UTF_8));
+//                })).map(httpClientResponse -> httpClientResponse.handler(buffer -> {
+//                    logger.log(System.Logger.Level.INFO, "Receive:" + new String(buffer.getBytes(), StandardCharsets.UTF_8));
                 }))
                 .onFailure().invoke(ex -> logger.log(System.Logger.Level.ERROR, "Ex:", ex))
                 .onItemOrFailure().invoke((itm, ex) -> client.closeAndForget());
