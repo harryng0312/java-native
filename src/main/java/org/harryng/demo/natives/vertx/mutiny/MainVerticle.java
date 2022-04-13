78f93a119206621b930ac2efed465accc155bc45
harryng <harry@videabiz.com>
2022-04-13 10:20:54 +0700

    - vertical

--- src/main/java/org/harryng/demo/natives/vertx/mutiny/MainVerticle.java
+++ src/main/java/org/harryng/demo/natives/vertx/mutiny/MainVerticle.java
@@ -1,37 +1,77 @@
 package org.harryng.demo.natives.vertx.mutiny;
 
+import io.smallrye.mutiny.Uni;
 import io.smallrye.mutiny.vertx.core.AbstractVerticle;
+import io.vertx.core.http.HttpServerOptions;
 import io.vertx.core.json.JsonObject;
+import io.vertx.mutiny.core.http.HttpServer;
 import io.vertx.mutiny.ext.web.Router;
 
 public class MainVerticle extends AbstractVerticle {
     static System.Logger logger = System.getLogger(MainVerticle.class.getCanonicalName());
+    private long counter = 0L;
+    private HttpServer server = null;
+
     @Override
-    public void start() throws Exception {
+    public Uni<Void> asyncStart() {
+        super.asyncStart();
+        // create a server
+        server = vertx.createHttpServer(new HttpServerOptions());
         // Create a Router
         Router router = Router.router(vertx);
-
-        // Mount the handler for all incoming requests at every path and HTTP method
-        router.route().handler(context -> {
-            // Get the address of the request
-            String address = context.request().connection().remoteAddress().toString();
-            // Get the query parameter "name"
-            var queryParams = context.queryParams();
-            String name = queryParams.contains("name") ? queryParams.get("name") : "unknown";
-            // Write a json response
-            context.json(new JsonObject().put("name", name).put("address", address).put("message", "Hello " + name + " connected from " + address));
-        });
+        router.get("/hello")
+                .produces("application/json")
+                .consumes("application/json")
+                .handler(context -> {
+                    // Get the address of the request
+                    String address = context.request().connection().remoteAddress().toString();
+                    // Get the query parameter "name"
+                    var queryParams = context.queryParams();
+                    String name = queryParams.contains("name") ? queryParams.get("name") : "unknown";
+                    // Write a json response
+//                    context.json(new JsonObject().put("name", name)
+//                                    .put("address", address)
+//                                    .put("message", "Hello " + name + " connected from " + address))
+//                            .subscribe().with(v -> {
+//                            });
+                    context.jsonAndForget(new JsonObject().put("name", name)
+                            .put("address", address)
+                            .put("message", "Hello " + name + " connected from " + address));
+//                    context.response()
+////                            .putHeader("content-type", "application/json")
+//                            .end(new JsonObject()
+//                                    .put("name", name)
+//                                    .put("address", address)
+//                                    .put("message", "Hello " + name + " connected from " + address)
+//                                    .toString())
+//                            .subscribe().with(v -> {
+//                            });
+                })
+                .failureHandler(context -> {
+                    context.response().setStatusCode(404).end()
+                            .subscribe().with(ex -> logger.log(System.Logger.Level.ERROR, "", ex));
+                });
 
         // Create the HTTP server
-        vertx.createHttpServer()
+        return server
                 // Handle every request using the router
                 .requestHandler(router)
                 // Start listening
-                .listen(8888)
+                .listen(8080)
                 // Print the port
-                .map(server -> {
+                .onItem().invoke(() -> {
                     logger.log(System.Logger.Level.INFO, "HTTP server started on port " + server.actualPort());
-                    return server;
-                });
+//                    return server;
+                })
+                .onFailure().invoke(ex -> logger.log(System.Logger.Level.ERROR, "", ex))
+                .replaceWithVoid();
+    }
+
+    @Override
+    public Uni<Void> asyncStop() {
+//        super.asyncStop()
+        logger.log(System.Logger.Level.INFO, "Vertx is shutting down!");
+//        vertx.close();
+        return super.asyncStop().flatMap(v -> server.close()).flatMap(v -> vertx.close());
     }
 }
--- src/main/java/org/harryng/demo/natives/vertx/mutiny/MainVertxCore.java
+++ src/main/java/org/harryng/demo/natives/vertx/mutiny/MainVertxCore.java
@@ -1,9 +1,12 @@
 package org.harryng.demo.natives.vertx.mutiny;
 
+import io.vertx.core.DeploymentOptions;
+import io.vertx.mutiny.core.Vertx;
+
 public class MainVertxCore {
     static System.Logger logger = System.getLogger(MainVertxCore.class.getCanonicalName());
 
-    public static void main(String[] args) {
+    public void doFileSystem(){
         var app = new MainVertxFileSystem();
 //        app.readBigFile();
 //        app.writeFile();
@@ -18,5 +21,21 @@ public class MainVertxCore {
 //                .subscribe().with(
 //                        item -> logger.log(System.Logger.Level.INFO, "vertx finished: " + item),
 //                        ex -> logger.log(System.Logger.Level.ERROR, "vertx error: ", ex));
+    }
+
+    public void doVertical(){
+        var vertx = Vertx.vertx();
+        var options = new DeploymentOptions()
+                .setWorker(false)
+                .setInstances(1);
+        logger.log(System.Logger.Level.INFO, "Deplopment starting...");
+        vertx.deployVerticleAndAwait(MainVerticle::new, options);
+        logger.log(System.Logger.Level.INFO, "Deplopment completed");
+//        Runtime.getRuntime().addShutdownHook(new Thread(vertx::close));
+    }
+
+    public static void main(String[] args) {
+        var main = new MainVertxCore();
+        main.doVertical();
     }
 }
